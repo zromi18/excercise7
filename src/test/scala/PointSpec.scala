aa48df00f96222ec245830b9eb3665165d3d8ba3
Roman Zarzecki <RZarzecki@pl.imshealth.com>
2016-05-25 09:29:55 +0200

    Fixing scala tests

--- src/test/scala/PointSpec.scala
+++ src/test/scala/PointSpec.scala
@@ -1,4 +1,3 @@
-import org.specs2.matcher.Matchers
 import org.scalatest._
 
 /**
@@ -9,7 +8,8 @@ class PointSpec extends FlatSpec with Matchers {
   "A Stack" should "pop values in last-in-first-out order" in {
     val point = Point(1, 2).move(2, 1)
 
-    assert((1 + 2) == 3)
+    val g: Int = 1
+    (g + 2) should be (3)
     assert(point == Point(3, 3))
   }
 }
